87fcdbcf09c4bfeafb034afce475e67c0b52fa56
Boris <boris.iv.iv@gmail.com>
2018-10-28 15:18:59 +0200

    II hw2 - heuristic implemented but still not working as expected.

--- II/homework2/hw2.cpp
+++ II/homework2/hw2.cpp
@@ -5,10 +5,14 @@
 using namespace std;
 
 const int N = 3;
+
 struct Node {
     int raw[N][N];
     int* operator[](int n) {
         return raw[n];
+    }
+    int at(int i, int j) const {
+        return raw[i][j];
     }
 };
 
@@ -36,14 +40,48 @@ struct NodeInfo
     }
 
     bool operator<(const NodeInfo& other) const {
-        return distance >= other.distance;
+        return *this > other;
     }
 
     // break some encapsulation
     int* operator[](int n) {
         return node[n];
     }
-    
+
+    int at(int i, int j) const {
+        return node.raw[i][j];
+    }
+
+public:
+    bool operator>(const NodeInfo& other) const {
+        int wthis = distance + this->getManh();
+        int wother = other.distance + other.getManh();
+
+        return wthis > wother;
+    }
+
+    int getManh() const {
+        int sum = 0;
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                pair<int, int> realPosition = make_pair(i, j);
+                pair<int, int> neededPosition;
+
+                if(at(i, j) == 0) {
+                    neededPosition = make_pair(N - 1, N - 1);
+                }
+                else {
+                    neededPosition = make_pair((at(i, j) - 1) / N, (at(i, j) - 1) % N);
+                }
+
+                sum += abs(neededPosition.first - realPosition.first) + abs(neededPosition.second - realPosition.second);
+            }
+        }
+
+        return sum;
+    }
+
 };
 
 NodeInfo start;
@@ -70,29 +108,29 @@ vector<NodeInfo> neighbors(NodeInfo node) {
     if(emptyPos.second + 1 < N) {
         // left
         neigh.push_back(NodeInfo());
-        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance. 
+        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance.
         swap(neigh[neigh.size() - 1][emptyPos.first][emptyPos.second + 1], neigh[neigh.size() - 1][emptyPos.first][emptyPos.second]);
     }
     if(emptyPos.second - 1 >= 0) {
         // right
         neigh.push_back(NodeInfo());
-        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance. 
+        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance.
         swap(neigh[neigh.size() - 1][emptyPos.first][emptyPos.second - 1], neigh[neigh.size() - 1][emptyPos.first][emptyPos.second]);
     }
     if(emptyPos.first + 1 < N) {
         // up
         neigh.push_back(NodeInfo());
-        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance. 
+        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance.
         swap(neigh[neigh.size() - 1][emptyPos.first + 1][emptyPos.second], neigh[neigh.size() - 1][emptyPos.first][emptyPos.second]);
     }
     if(emptyPos.first - 1 >= 0) {
         // down
         neigh.push_back(NodeInfo());
-        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance. 
+        neigh[neigh.size() - 1].copyNode(node); // ugly code. Maybe refactoring in the future. For now is better performance.
         swap(neigh[neigh.size() - 1][emptyPos.first - 1][emptyPos.second], neigh[neigh.size() - 1][emptyPos.first][emptyPos.second]);
     }
- 
-    return neigh;   
+
+    return neigh;
 }
 
 void solve() {
@@ -126,9 +164,9 @@ void solve() {
 int main() {
 
     int st[N][N] = {
-        {2, 3, 6},
-        {1, 0, 7},
-        {4, 8, 5}
+        {6, 2, 3},
+        {1, 4, 8},
+        {7, 0, 5}
     };
     int end[N][N] = {
         {1, 2, 3},
@@ -136,12 +174,17 @@ int main() {
         {7, 8, 0}
     };
 
+    // 1,1 - 2,1 = 1
+    // 2,1 - 1,1 = 1
+
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
             start[i][j] = st[i][j];
             final[i][j] = end[i][j];
         }
     }
+
+    cout << start.getManh() << endl;
 
     solve();
 
